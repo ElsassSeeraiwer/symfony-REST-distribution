724bcea99122dd26b3e9f3525690f7d44b5838bb
keyermann <kevin.eyermann@ign.fr>
2013-06-07 10:12:58 +0200

    Simplifications

--- src/ElsassSeeraiwer/RestBundle/Controller/UsersController.php
+++ src/ElsassSeeraiwer/RestBundle/Controller/UsersController.php
@@ -6,77 +6,46 @@ namespace ElsassSeeraiwer\RestBundle\Controller;
 //use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 //use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\FOSRestController;
-//use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use ElsassSeeraiwer\ESDemoBundle\Entity\User;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class UsersController extends FOSRestController
 {
-    public function optionsUsersAction()
-    {} // "options_users" [OPTIONS] /users
-
     /**
      * @ApiDoc(
      *  resource=true,
      *  description="Renvoie la liste des utilisateurs"
      * )
+     *
+     * @Rest\View
      */
     public function getUsersAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository('ElsassSeeraiwerESDemoBundle:User')->findAll();
+        $entities = $em->getRepository('ElsassSeeraiwerESDemoBundle:User')->findAll();
 
-        $view = $this->view($data, 200)
-            ->setTemplateVar('users')
-        ;
-
-        return $this->handleView($view);
+        return array(
+            'users' => $entities
+        );
     } // "get_users"     [GET] /users
-
-    public function newUsersAction()
-    {} // "new_users"     [GET] /users/new
-
-    public function postUsersAction()
-    {} // "post_users"    [POST] /users
-
-    public function patchUsersAction()
-    {} // "patch_users"   [PATCH] /users
 
     /**
      * @ApiDoc(
      *  description="Renvoie l'utilisateur par son nom d'utilisateur"
      * )
+     *
+     * @Rest\View
      */
     public function getUserAction($username)
     {
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository('ElsassSeeraiwerESDemoBundle:User')->findByUsername($username);
+        $entity = $em->getRepository('ElsassSeeraiwerESDemoBundle:User')->findOneByUsername($username);
 
-        $view = $this->view($data, 200)
-            ->setTemplateVar('users')
-        ;
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find user entity');
+        }
 
-        return $this->handleView($view);
-    } // "get_user"      [GET] /users/{slug}
-
-    public function editUserAction($slug)
-    {} // "edit_user"     [GET] /users/{slug}/edit
-
-    public function putUserAction($slug)
-    {} // "put_user"      [PUT] /users/{slug}
-
-    public function patchUserAction($slug)
-    {} // "patch_user"    [PATCH] /users/{slug}
-
-    public function lockUserAction($slug)
-    {} // "lock_user"     [PATCH] /users/{slug}/lock
-
-    public function banUserAction($slug)
-    {} // "ban_user"      [PATCH] /users/{slug}/ban
-
-    public function removeUserAction($slug)
-    {} // "remove_user"   [GET] /users/{slug}/remove
-
-    public function deleteUserAction($slug)
-    {} // "delete_user"   [DELETE] /users/{slug}
+        return $entity;
+    } // "get_user"      [GET] /users/{username}
 }
